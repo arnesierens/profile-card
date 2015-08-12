80c87481d5cef589317d39ca577ec33001e7fee0
Arne Sierens <arne.srns@gmail.com>
2015-08-12 21:17:03 +0200

    Added enquire.js to add/delete particles on window resize

--- js/main.js
+++ js/main.js
@@ -1,63 +1,66 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    if(document.documentElement.clientWidth > 412) {
-        particlesJS("particles-js", {
-            "particles": {
-                "number": {
-                    "value": 80,
-                    "density": {
-                      "enable": true,
-                      "value_area": 800
+    enquire.register("screen and (min-width: 375px)", {
+        match : function() {
+            // initialize after destroypJS()
+            window.pJSDom = [];
+            
+            particlesJS("particles-js", {
+                "particles": {
+                    "number": {
+                        "value": 80,
+                        "density": {
+                          "enable": true,
+                          "value_area": 800
+                        }
+                    },
+                    "color": {
+                        "value": "#ffffff"
+                    },
+                    "shape": {
+                        "type": "circle",
+                        "polygon": {
+                            "nb_sides": 5
+                        }
+                    },
+                    "opacity": {
+                        "value": 0.5,
+                        "random": false
+                    },
+                    "size": {
+                        "value": 3,
+                        "random": true
+                    },
+                    "line_linked": {
+                        "enable": false
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 0.3,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false
                     }
                 },
-                "color": {
-                    "value": "#ffffff"
-                },
-                "shape": {
-                    "type": "circle",
-                    "polygon": {
-                        "nb_sides": 5
+                "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                        "onhover": {
+                            "enable": false
+                        },
+                        "onclick": {
+                            "enable": false
+                        },
+                        "resize": true
                     }
                 },
-                "opacity": {
-                    "value": 0.5,
-                    "random": false
-                },
-                "size": {
-                    "value": 3,
-                    "random": true
-                },
-                "line_linked": {
-                    "enable": false,
-                    "distance": 150,
-                    "color": "#3b5d92",
-                    "opacity": 0.5,
-                    "width": 1
-                },
-                "move": {
-                    "enable": true,
-                    "speed": 0.3,
-                    "direction": "none",
-                    "random": false,
-                    "straight": false,
-                    "out_mode": "out",
-                    "bounce": false
-                }
-            },
-            "interactivity": {
-                "detect_on": "canvas",
-                "events": {
-                    "onhover": {
-                        "enable": false,
-                        "mode": "repulse"
-                    },
-                    "onclick": {
-                        "enable": false,
-                        "mode": "push"
-                    },
-                    "resize": true
-                }
-            },
-            "retina_detect": true
-        });
-    }
+                "retina_detect": true
+            }); 
+        },  
+        unmatch : function() {
+            // pJS.fn.vendors.destroypJS not working
+            pJSDom[0]['pJS'].fn.vendors.destroypJS();
+        }
+    });    
 });
