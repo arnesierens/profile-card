934ff5706463a44697bdf39f961738ff00812817
Arne Sierens <arne.srns@gmail.com>
2015-08-12 19:55:42 +0200

    Added check if DOM is fully loaded and parsed before particles is executed

--- js/main.js
+++ js/main.js
@@ -1,61 +1,63 @@
-if(document.documentElement.clientWidth > 412) {
-    particlesJS("particles-js", {
-        "particles": {
-            "number": {
-                "value": 80,
-                "density": {
-                  "enable": true,
-                  "value_area": 800
+document.addEventListener('DOMContentLoaded', function(event) {
+    if(document.documentElement.clientWidth > 412) {
+        particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 80,
+                    "density": {
+                      "enable": true,
+                      "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "circle",
+                    "polygon": {
+                        "nb_sides": 5
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false
+                },
+                "size": {
+                    "value": 3,
+                    "random": true
+                },
+                "line_linked": {
+                    "enable": false,
+                    "distance": 150,
+                    "color": "#3b5d92",
+                    "opacity": 0.5,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 0.3,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false
                 }
             },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle",
-                "polygon": {
-                    "nb_sides": 5
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": false,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "push"
+                    },
+                    "resize": true
                 }
             },
-            "opacity": {
-                "value": 0.5,
-                "random": false
-            },
-            "size": {
-                "value": 3,
-                "random": true
-            },
-            "line_linked": {
-                "enable": false,
-                "distance": 150,
-                "color": "#3b5d92",
-                "opacity": 0.5,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 0.3,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false,
-                    "mode": "repulse"
-                },
-                "onclick": {
-                    "enable": false,
-                    "mode": "push"
-                },
-                "resize": true
-            }
-        },
-        "retina_detect": true
-    });
-}
+            "retina_detect": true
+        });
+    }
+});
